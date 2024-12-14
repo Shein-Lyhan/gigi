39458105af8c0d9542a22236fd2f6698647dd035
Shein-Lyhan <66256773+Shein-Lyhan@users.noreply.github.com>
2024-12-14 10:57:12 +0630

    Update kg.py

--- kg.py
+++ kg.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.header("Hello GiGi")
-st.subheader("Nice to meet you")
+st.header("Happy Birthday, My GiGi !")
+st.subheader("
+Thank you for everything you do for me and Eddie. Your strength, love, and care are the foundation of our happiness. I hope this year brings you endless joy, laughter, and cherished moments. You deserve the world and more. Love you always, and thank you for being our rock during every moment.")
